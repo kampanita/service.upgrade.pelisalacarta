ea2dff0c18cae50b798fd8512acf473970570849
kampanita <kampanita@gmail.com>
2016-03-22 15:28:09 +0100

    Update service.py
    Añado algunas notificaciones para saber que esta haciendo.

--- service.py
+++ service.py
@@ -22,10 +22,16 @@ notify = addon.getSetting('notify')
 
 time = 7000 #in miliseconds
 time2= 100
+num_files=0
+num_files2=0
 
 def copydir(source, dest, indent = 0):
     notify2 = addon.getSetting('notify2')
     """Copy a directory structure overwriting existing files"""
+    num_files2=0
+    dialog  = xbmcgui.DialogProgressBG()
+    dialog.create('UpdPelisAlacarta - copy files')
+    
     for root, dirs, files in os.walk(source):
         if not os.path.isdir(root):
             os.makedirs(root)
@@ -34,9 +40,15 @@ def copydir(source, dest, indent = 0):
             dest_path = os.path.join(dest, rel_path, each_file)
             xbmc.log(os.path.join(root, each_file)+' -> '+dest_path)
             shutil.copyfile(os.path.join(root, each_file), dest_path)
-            if notify2:
-                xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(addonname,each_file, time2, icon))
+            num_files2+=1
             
+            if notify2:
+                #xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(addonname,each_file, time2, icon))                                                
+                dialog.update(int(num_files2/num_files)*100,rel_path,each_file)
+                
+    dialog.update(-1)
+    dialog.close
+    
 def upgrade():  
 
     notify = addon.getSetting('notify') 
@@ -67,22 +79,29 @@ def upgrade():
            #urllib.urlretrieve ("https://codeload.github.com/tvalacarta/pelisalacarta/zip/master",  xbmc.translatePath(path+'pelis.zip'))
            #
            ###
-           #  Cambio la manera de bajarme el zip
+           
            url = "https://codeload.github.com/tvalacarta/pelisalacarta/zip/master"
            f = urllib2.urlopen(url)
            with open(xbmc.translatePath(path+'pelis.zip'), "wb") as code: 
                code.write(f.read())
                code.close()
-           ### 
+           if notify:
+               xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(addonname,"DOWNLOADED", time, icon))                           
+           ###
+           num_files=0
            fh = open( xbmc.translatePath(path+'pelis.zip'), 'rb')
            z = zipfile.ZipFile(fh)
-          
+           
            for name in z.namelist():
                if "main-classic" in str(name):
                    
                    z.extract(name,xbmc.translatePath(path))                
-                 
+                   num_files+=1
+                   
            fh.close()
+           
+           if notify:
+               xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(addonname,"Files extracted from zip", time, icon))            
            
            ori = xbmc.translatePath(path+'pelisalacarta-master/python/main-classic')
            dest =  xbmc.translatePath(path2+'addons/plugin.video.pelisalacarta')
